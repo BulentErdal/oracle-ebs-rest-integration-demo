c0d99f8a2a42b78464f412b65c18720b1bbc89cf
BulentErdal <100426485+BulentErdal@users.noreply.github.com>
2026-03-20 15:53:52 +0300

    comiit
    Added new project

---
+++ src/main/java/com/bulent/integration/demo/OracleEbsRestIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bulent.integration.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleEbsRestIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleEbsRestIntegrationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bulent/integration/demo/OracleEbsRestIntegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulent.integration.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleEbsRestIntegrationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
